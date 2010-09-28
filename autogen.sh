12ea0796d1dc17127fe293a13b6b2c52fa2720a4
Gabriel Martinez <reitaka@gmail.com>
2010-09-27 22:03:36 -0400

    Added build files from opencl-apps

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# Copyright 2010 by Virginia Polytechnic Institute and State
+# University. All rights reserved. Virginia Polytechnic Institute and
+# State University (Virginia Tech) owns the software and its
+# associated documentation.
+#
+
+autoreconf -vif
