8a93847af69684b8b21801fe19d23477d4021f8f
Benjamin Wang <benwang@vt.edu>
2011-04-01 10:21:24 -0400

    update bsort

--- graph-traversal/bitonic-sort/main.c
+++ graph-traversal/bitonic-sort/main.c
@@ -1,16 +1,8 @@
-/*
- * File:   main.c
- * Author: ben
- *
- * Created on October 21, 2010, 8:18 PM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <unistd.h>
 #include <CL/cl.h>
-
-#define SHARED_SIZE_LIMIT 512
 
 #define CHKERR(err, str) \
     if (err != CL_SUCCESS) \
@@ -18,22 +10,31 @@
         fprintf(stderr, "CL Error %d: %s\n", err, str); \
         exit(1); \
     }
+
+
+
 #define USEGPU 1
+#define MAX_VALUE 65536
 
 int compare(const void * a, const void * b) {
     return ( *(int*) a - *(int*) b);
 }
 
+void usage() {
+    printf("bsort [ns]\n");
+    printf("n <number> - number of items to be sorted( must be power of 2\n");
+    printf("s <seed>  - set the seed for the random number\n");
+}
+
 int main(int argc, char** argv) {
     cl_int err;
-    unsigned int *h_InputKey, *h_InputVal, *h_OutputKeyGPU, *h_OutputValGPU;
-    cl_mem d_InputKey, d_InputVal, d_OutputKey, d_OutputVal;
-    unsigned int hTimer, batchSize;
-    unsigned int i, j, factorizationRemainder, blockCount, threadCount, arrayLength = 64U, glo = 128;
-    int flag = 1, dir = 1;
-    unsigned int log2L;
+    unsigned int i, j, k;
+    int c;
 
-    size_t local_size, global_size, local;
+    const unsigned int numValues = MAX_VALUE;
+
+    size_t global_size;
+    size_t local_size, max_local, i_, j_;
 
     cl_platform_id platform_id;
     cl_device_id device_id;
@@ -41,161 +42,179 @@ int main(int argc, char** argv) {
     cl_command_queue commands;
     cl_program program;
     cl_kernel kernel;
+    cl_event events[2];
 
 
+    cl_mem input_mem;
 
     FILE *kernelFile;
     char *kernelSource;
     size_t kernelLength;
-    printf(" Starting...\n\n");
+    size_t lengthRead;
 
-    const unsigned int N = 512;
-    const unsigned int DIR = 0;
-    const unsigned int numValues = 65536;
-    const unsigned int numIterations = 1;
-    printf("Allocating and initializing host arrays...\n\n");
-    h_InputKey = (unsigned int *) malloc(N * sizeof (unsigned int));
-    h_InputVal = (unsigned int *) malloc(N * sizeof (unsigned int));
-    h_OutputKeyGPU = (unsigned int *) malloc(N * sizeof (unsigned int));
-    h_OutputValGPU = (unsigned int *) malloc(N * sizeof (unsigned int));
-    srand(2001);
-    for (i = 0; i < N; i++) {
-        h_InputKey[i] = rand() % numValues;
-        h_InputVal[i] = i;
+    unsigned int* input;
+    unsigned int* output;
+    unsigned int size = 512;
+    int seed = 2010;
+    while ((c = getopt(argc, argv, "h:n:s")) != -1) {
+        switch (c) {
+            case 'h':
+                usage();
+                exit(0);
+                break;
+            case 'n':
+                size = atoi(optarg);
+                break;
+            case 's':
+                seed = atoi(optarg);
+                break;
+            default:
+                abort();
+        }
     }
-    for (i = 0; i < N; i++)
-        printf("%u  ", h_InputKey[i]);
-    printf("Running GPU bitonic sort...\n\n");
+    if (size <= 1) {
+        usage();
+        abort();
+    }
+    for (i = size; i > 1; i /= 2) {
+        if (i % 2 ==1) {
+            usage();
+            abort();
+        }
 
-    dir = (dir != 0);
-    batchSize = N / arrayLength;
-    blockCount = /*batchSize * arrayLength*/N / SHARED_SIZE_LIMIT;
-    threadCount = SHARED_SIZE_LIMIT / 2;
-    //assert((batchSize * arrayLength) % SHARED_SIZE_LIMIT == 0);
-    glo = threadCount*blockCount;
-    global_size = glo;
-    local = threadCount;
+    }
 
-    //glo/=2;
+    input = malloc(sizeof (unsigned int) * size);
+    output = malloc(sizeof (unsigned int) * size);
+    /* Fill input set  */
+    srand(seed);
+    for (i = 0; i < size; i++) {
+        input [i] = rand() % numValues;
+        output[i] = input[i];
+    }
+    for (i = 0; i < size; i++)
+        printf("%u  ", input[i]);
+    printf("\nRunning GPU bitonic sort...\n\n");
 
+
+
+    /* Retrieve an OpenCL platform */
     err = clGetPlatformIDs(1, &platform_id, NULL);
     CHKERR(err, "Failed to get a platform!");
 
-
+    /* Connect to a compute device */
     err = clGetDeviceIDs(platform_id, USEGPU ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
     CHKERR(err, "Failed to create a device group!");
 
-
+    /* Create a compute context */
     context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
     CHKERR(err, "Failed to create a compute context!");
 
-
+    /* Create a command queue */
     commands = clCreateCommandQueue(context, device_id, 0, &err);
     CHKERR(err, "Failed to create a command queue!");
 
-
-    kernelFile = fopen("bitonicSort.cl", "r");
+    /* Load kernel source */
+    kernelFile = fopen("sort.cl", "r");
     fseek(kernelFile, 0, SEEK_END);
     kernelLength = (size_t) ftell(kernelFile);
     kernelSource = (char *) malloc(sizeof (char) *kernelLength);
     rewind(kernelFile);
-    fread((void *) kernelSource, kernelLength, 1, kernelFile);
+    lengthRead = fread((void *) kernelSource, kernelLength, 1, kernelFile);
     fclose(kernelFile);
 
-
+    /* Create the compute program from the source buffer */
     program = clCreateProgramWithSource(context, 1, (const char **) &kernelSource, &kernelLength, &err);
     CHKERR(err, "Failed to create a compute program!");
 
+
+    /* Free kernel source */
     free(kernelSource);
 
-
+    /* Build the program executable */
     err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     if (err == CL_BUILD_PROGRAM_FAILURE) {
-        char *log;
+        char *buildLog;
         size_t logLen;
         err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &logLen);
-        log = (char *) malloc(sizeof (char) *logLen);
-        err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, logLen, (void *) log, NULL);
-        fprintf(stderr, "CL Error %d: Failed to build program! Log:\n%s", err, log);
-        free(log);
+        buildLog = (char *) malloc(sizeof (char) *logLen);
+        err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, logLen, (void *) buildLog, NULL);
+        fprintf(stderr, "CL Error %d: Failed to build program! Log:\n%s", err, buildLog);
+        free(buildLog);
         exit(1);
     }
     CHKERR(err, "Failed to build program!");
 
-
-    kernel = clCreateKernel(program, "bitonicSortShared", &err);
+    /* Create the compute kernel in the program we wish to run */
+    kernel = clCreateKernel(program, "sort", &err);
     CHKERR(err, "Failed to create a compute kernel!");
 
-    d_InputKey = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof (unsigned int) *N, NULL, &err);
-    CHKERR(err, "Failed to Create device buffer!");
 
-    d_InputVal = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof (unsigned int) *N, NULL, &err);
-    CHKERR(err, "Failed to Create device buffer!");
+    /* Create the input and output arrays in device memory for our calculation */
+    input_mem = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof (unsigned int) *size, NULL, &err);
+    CHKERR(err, "Failed to allocate device memory!");
 
-    d_OutputKey = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof (unsigned int) *N, NULL, &err);
-    CHKERR(err, "Failed to Create device buffer!");
 
-    d_OutputVal = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof (unsigned int) *N, NULL, &err);
-    CHKERR(err, "Failed to Create  device buffer!");
+    /* Write our data set into the input array in device memory */
+    err = clEnqueueWriteBuffer(commands, input_mem, CL_TRUE, 0, sizeof (unsigned int) *size, input, 0, NULL, NULL);
+    CHKERR(err, "Failed to write to source array!");
 
-    err = clEnqueueWriteBuffer(commands, d_InputKey, CL_TRUE, 0, sizeof (unsigned int) *N, (void *) h_InputKey, 0, NULL, NULL);
-    CHKERR(err, "Failed to Enqueue  device buffer!");
 
-    err = clEnqueueWriteBuffer(commands, d_InputVal, CL_TRUE, 0, sizeof (unsigned int) *N, (void *) h_InputVal, 0, NULL, NULL);
-    CHKERR(err, "Failed to Enqueue  device buffer!");
-    for (arrayLength = 64; arrayLength <= N; arrayLength *= 2) {
-        batchSize = N / arrayLength;
-        blockCount = /*batchSize * arrayLength*/N / SHARED_SIZE_LIMIT;
-        threadCount = SHARED_SIZE_LIMIT / 2;
-        //assert((batchSize * arrayLength) % SHARED_SIZE_LIMIT == 0);
-        glo = threadCount*blockCount;
-        global_size = glo;
-        local = threadCount;
+    /* Execute the kernel over the entire range of our 1d input data set */
+    /* Get the maximum work group size for executing the kernel on the device */
+    err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof (size_t), (void *) &max_local, NULL);
+    CHKERR(err, "Failed to retrieve kernel work group info!");
+    for (local_size = 1024; local_size >= 1; local_size /= 2) {
+        if (max_local >= local_size) {
+            break;
+        }
+    }
+    global_size = size;
+    if (global_size < local_size)
+        local_size = global_size;
+    /* Timer starts here*/
+    /* Set the arguments to our compute kernel */
+    err = clSetKernelArg(kernel, 0, sizeof (cl_mem), &input_mem);
+    CHKERR(err, "Failed to set kernel arguments!");
+    for (i = 2; i <= size; i *= 2) {
+        for (j = i / 2; j > 0; j /= 2) {
+            err = clSetKernelArg(kernel, 1, sizeof (unsigned int), (void *) &j);
+            err |= clSetKernelArg(kernel, 2, sizeof (unsigned int), (void *) &i);
+            err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global_size, &local_size, 0, NULL, &events [0]);
+            CHKERR(err, "Failed to execute kernel!");
+            err = clWaitForEvents(1, &events [0]);
+            CHKERR(err, "Failed to execute event");
 
-        err = clSetKernelArg(kernel, 0, sizeof (cl_mem), (void *) &d_OutputKey);
-        err |= clSetKernelArg(kernel, 1, sizeof (cl_mem), (void *) &d_OutputVal);
-        err |= clSetKernelArg(kernel, 2, sizeof (cl_mem), (void *) &d_InputKey);
-        err |= clSetKernelArg(kernel, 3, sizeof (cl_mem), (void *) &d_InputVal);
-        err |= clSetKernelArg(kernel, 4, sizeof (unsigned int), (void *) &arrayLength);
-        err |= clSetKernelArg(kernel, 5, sizeof (unsigned int), (void *) &dir);
-        CHKERR(err, "Failed to set kernel arguments!");
-
-        err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof (size_t), (void *) &local_size, NULL);
-        CHKERR(err, "Failed to retrieve kernel work group info!");
-
-        err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global_size, &local, 0, NULL, NULL);
-        CHKERR(err, "Failed to execute kernel!");
+        }
     }
 
-    err = clEnqueueReadBuffer(commands, d_OutputKey, CL_TRUE, 0, sizeof (unsigned int) *N, h_OutputKeyGPU, 0, NULL, NULL);
-    CHKERR(err, "Failed to read output array!");
+    /* Wait for the command commands to get serviced before reading back results */
+    clFinish(commands);
 
-    err = clEnqueueReadBuffer(commands, d_OutputVal, CL_TRUE, 0, sizeof (unsigned int) *N, h_OutputValGPU, 0, NULL, NULL);
+    /* Read back the results from the device to verify the output */
+    err = clEnqueueReadBuffer(commands, input_mem, CL_TRUE, 0, sizeof (unsigned int) * size, input, 0, NULL, NULL);
     CHKERR(err, "Failed to read output array!");
+    /* timer ends here */
 
-    qsort(h_InputKey, N, sizeof (unsigned int), compare);
-    for (i = 0; i < N; i++)
-        printf("%u  ", h_OutputKeyGPU[i]);
-    for (i = 0; i < N; i++)
-        if (h_OutputKeyGPU[i] != h_InputKey[i]){
+    /* Validate our results */
+    qsort(output, size, sizeof (unsigned int), compare);
+    for (i = 0; i < size; i++)
+        if (input[i] != output[i]) {
             printf("The result is invalid");
             exit(1);
         }
+    /* Print a brief summary detailing the results */
+    for (i = 0; i < size; i++)
+        printf("%u  ", input[i]);
 
-   
-    free(h_OutputValGPU);
-    free(h_OutputKeyGPU);
-    free(h_InputVal);
-    free(h_InputKey);
-    clReleaseMemObject(d_InputKey);
-    clReleaseMemObject(d_InputVal);
-    clReleaseMemObject(d_OutputKey);
-    clReleaseMemObject(d_OutputVal);
+    printf("The result is valid\n");
+
+    /* Shutdown and cleanup */
+    clReleaseMemObject(input_mem);
     clReleaseProgram(program);
     clReleaseKernel(kernel);
     clReleaseCommandQueue(commands);
     clReleaseContext(context);
-    return (EXIT_SUCCESS);
+
+    return 0;
 }
-
-
