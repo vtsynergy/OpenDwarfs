00af64edfbf08701d9825b1982203a5d6db58474
Kenneth Lee <klee1@vt.edu>
2011-05-27 12:36:56 -0400

    Cleaned up the spacing of the file

--- combinational-logic/crc/crc_algo.c
+++ combinational-logic/crc/crc_algo.c
@@ -13,11 +13,11 @@
 #endif
 
 #define CHKERR(err, str) \
-    if (err != CL_SUCCESS) \
-    { \
-        fprintf(stderr, "CL Error %d: %s\n", err, str); \
-        exit(1); \
-    }
+	if (err != CL_SUCCESS) \
+	{ \
+		fprintf(stderr, "CL Error %d: %s\n", err, str); \
+		exit(1); \
+	}
 
 #define USEGPU 1
 #define DATA_SIZE 100000000
@@ -30,7 +30,7 @@ cl_context context;
 cl_command_queue commands;
 cl_program program;
 cl_kernel kernel_compute;
-    
+	
 cl_mem dev_input;
 cl_mem dev_table;
 cl_mem dev_output;
@@ -39,36 +39,36 @@ cl_mem dev_output;
 void usage()
 {
 	printf("graphCreator [hsivp]\n");
-	printf("h         - print this help message\n");
+	printf("h		 - print this help message\n");
 	printf("s <seed>  - set the seed for the vertex\n");
 	printf("i <file>  - take input from file instead of randomly generating code\n");
-	printf("v         - verify parallel code with serial implementation of crc\n");
+	printf("v		 - verify parallel code with serial implementation of crc\n");
 	printf("p <int>   - change the last 8 bits of the crc polynomial\n");
 }
 
 unsigned char serialCRC(unsigned char* h_num, size_t size, unsigned char crc)
 {
-    unsigned int i;
-    unsigned char num = h_num[0];
-    for(i = 1; i < size + 1; i++)
-    {
-        unsigned char crcCalc = h_num[i];
-        unsigned int k;
-        if(i == size)
-            crcCalc = 0;
-        for(k = 0; k < 8; k++)
-        {
-            //If the k-th bit is 1
-            if((num >> (7-k)) % 2 == 1)
-            {
-                num ^= crc >> (k + 1);
-                crcCalc ^= crc << (7-k);
-            }
-        }
-        num = crcCalc;
-    }
+	unsigned int i;
+	unsigned char num = h_num[0];
+	for(i = 1; i < size + 1; i++)
+	{
+		unsigned char crcCalc = h_num[i];
+		unsigned int k;
+		if(i == size)
+			crcCalc = 0;
+		for(k = 0; k < 8; k++)
+		{
+			//If the k-th bit is 1
+			if((num >> (7-k)) % 2 == 1)
+			{
+				num ^= crc >> (k + 1);
+				crcCalc ^= crc << (7-k);
+			}
+		}
+		num = crcCalc;
+	}
 
-    return num;
+	return num;
 }
 
 int getNextChunk(long N, FILE* fp, char* h_num, size_t maxSize, size_t* read, int* pad_o)
@@ -144,39 +144,39 @@ void computeTables(unsigned char* tables, int numTables, unsigned char crc)
 
 unsigned char computeCRCGPU(unsigned char* h_num, unsigned long N, unsigned char crc, unsigned char* tables, unsigned long numTables)
 {
-    // Write our data set into the input array in device memory
-    int err = clEnqueueWriteBuffer(commands, dev_input, CL_TRUE, 0, sizeof(char)*N, h_num, 0, NULL, NULL);
-    CHKERR(err, "Failed to write to source array!");
+	// Write our data set into the input array in device memory
+	int err = clEnqueueWriteBuffer(commands, dev_input, CL_TRUE, 0, sizeof(char)*N, h_num, 0, NULL, NULL);
+	CHKERR(err, "Failed to write to source array!");
 
-    // Set the arguments to our compute kernel
-    err = 0;
-    err = clSetKernelArg(kernel_compute, 0, sizeof(cl_mem), &dev_input);
-    err |= clSetKernelArg(kernel_compute, 1, sizeof(cl_mem), &dev_table);
+	// Set the arguments to our compute kernel
+	err = 0;
+	err = clSetKernelArg(kernel_compute, 0, sizeof(cl_mem), &dev_input);
+	err |= clSetKernelArg(kernel_compute, 1, sizeof(cl_mem), &dev_table);
 	err |= clSetKernelArg(kernel_compute, 2, sizeof(unsigned int), &numTables);
-    err |= clSetKernelArg(kernel_compute, 3, sizeof(cl_mem), &dev_output);
-    err |= clSetKernelArg(kernel_compute, 4, sizeof(unsigned int), &N);
-    CHKERR(err, "Failed to set compute kernel arguments!");
+	err |= clSetKernelArg(kernel_compute, 3, sizeof(cl_mem), &dev_output);
+	err |= clSetKernelArg(kernel_compute, 4, sizeof(unsigned int), &N);
+	CHKERR(err, "Failed to set compute kernel arguments!");
 
 	size_t local_size;
 	size_t global_size;
-    // Get the maximum work group size for executing the kernel on the device
-    err = clGetKernelWorkGroupInfo(kernel_compute, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(size_t), (void *) &local_size, NULL);
-    CHKERR(err, "Failed to retrieve kernel_compute work group info!");
+	// Get the maximum work group size for executing the kernel on the device
+	err = clGetKernelWorkGroupInfo(kernel_compute, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(size_t), (void *) &local_size, NULL);
+	CHKERR(err, "Failed to retrieve kernel_compute work group info!");
 
-    // Wait for the command commands to get serviced before reading back results
-    clFinish(commands);
-    
+	// Wait for the command commands to get serviced before reading back results
+	clFinish(commands);
+	
 	// Execute the kernel over the entire range of our 1d input data set
-    // using the maximum number of work group items for this device
-    global_size = N;
-    local_size = MIN(local_size, N);
-    err = clEnqueueNDRangeKernel(commands, kernel_compute, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
-    CHKERR(err, "Failed to execute compute kernel!");
+	// using the maximum number of work group items for this device
+	global_size = N;
+	local_size = MIN(local_size, N);
+	err = clEnqueueNDRangeKernel(commands, kernel_compute, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
+	CHKERR(err, "Failed to execute compute kernel!");
 
 	unsigned char* h_answer = malloc(sizeof(*h_answer)*N);
-    // Read back the results from the device to verify the output
-    err = clEnqueueReadBuffer(commands, dev_output, CL_TRUE, 0, sizeof(char)*N, h_answer, 0, NULL, NULL);
-    CHKERR(err, "Failed to read output array!");
+	// Read back the results from the device to verify the output
+	err = clEnqueueReadBuffer(commands, dev_output, CL_TRUE, 0, sizeof(char)*N, h_answer, 0, NULL, NULL);
+	CHKERR(err, "Failed to read output array!");
 
 	unsigned char answer = 0;
 	
@@ -211,21 +211,21 @@ unsigned char computeCRC(unsigned char* h_num, unsigned long N, unsigned char cr
 
 void setupGPU()
 {
-    // Retrieve an OpenCL platform
-    int err = clGetPlatformIDs(1, &platform_id, NULL);
-    CHKERR(err, "Failed to get a platform!");
+	// Retrieve an OpenCL platform
+	int err = clGetPlatformIDs(1, &platform_id, NULL);
+	CHKERR(err, "Failed to get a platform!");
 
-    // Connect to a compute device
-    err = clGetDeviceIDs(platform_id, USEGPU ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-    CHKERR(err, "Failed to create a device group!");
+	// Connect to a compute device
+	err = clGetDeviceIDs(platform_id, USEGPU ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
+	CHKERR(err, "Failed to create a device group!");
 
-    // Create a compute context
-    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-    CHKERR(err, "Failed to create a compute context!");
+	// Create a compute context
+	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+	CHKERR(err, "Failed to create a compute context!");
 
-    // Create a command queue
-    commands = clCreateCommandQueue(context, device_id, 0, &err);
-    CHKERR(err, "Failed to create a command queue!");
+	// Create a command queue
+	commands = clCreateCommandQueue(context, device_id, 0, &err);
+	CHKERR(err, "Failed to create a command queue!");
 
 	FILE* kernelFile = NULL;
 	kernelFile = fopen(KernelSourceFile, "r");
@@ -238,51 +238,51 @@ void setupGPU()
 	fread((void *) kernelSource, kernelLength, 1, kernelFile);
 	kernelSource[kernelLength] = 0;
 	fclose(kernelFile);
-    
+	
 	// Create the compute program from the source buffer
-    program = clCreateProgramWithSource(context, 1, (const char **) &kernelSource, NULL, &err);
-    CHKERR(err, "Failed to create a compute program!");
+	program = clCreateProgramWithSource(context, 1, (const char **) &kernelSource, NULL, &err);
+	CHKERR(err, "Failed to create a compute program!");
 
-    free(kernelSource);
+	free(kernelSource);
 
-    // Build the program executable
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    if (err == CL_BUILD_PROGRAM_FAILURE)
-    {
-        char *log;
-        size_t logLen;
-        err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &logLen);
-        log = (char *) malloc(sizeof(char)*logLen);
-        err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, logLen, (void *) log, NULL);
-        fprintf(stderr, "CL Error %d: Failed to build program! Log:\n%s", err, log);
-        free(log);
-        exit(1);
-    }
-    CHKERR(err, "Failed to build program!");
+	// Build the program executable
+	err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (err == CL_BUILD_PROGRAM_FAILURE)
+	{
+		char *log;
+		size_t logLen;
+		err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &logLen);
+		log = (char *) malloc(sizeof(char)*logLen);
+		err = clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, logLen, (void *) log, NULL);
+		fprintf(stderr, "CL Error %d: Failed to build program! Log:\n%s", err, log);
+		free(log);
+		exit(1);
+	}
+	CHKERR(err, "Failed to build program!");
 
-    // Create the compute kernel in the program we wish to run
-    kernel_compute = clCreateKernel(program, "compute", &err);
-    CHKERR(err, "Failed to create a compute kernel!");
+	// Create the compute kernel in the program we wish to run
+	kernel_compute = clCreateKernel(program, "compute", &err);
+	CHKERR(err, "Failed to create a compute kernel!");
 }
 
 int main(int argc, char** argv)
 {
-    cl_int err;
+	cl_int err;
 
 	unsigned char* h_num;
 	unsigned char* h_answer;
-    unsigned char* data = NULL;
-    unsigned char h_table[256];
-    unsigned char crc = 0x9B;
+	unsigned char* data = NULL;
+	unsigned char crc = 0x9B;
 	unsigned long N = 1024;
-    unsigned char finalCRC;
+	unsigned char finalCRC;
+	unsigned char* h_tables;
 	unsigned int run_serial = 0;
 	char* file = NULL;
 	size_t maxSize = DATA_SIZE;
 	size_t read = 0;
 	FILE* fp = NULL;
 	unsigned int seed = time(NULL);
-    srand(seed);
+	srand(seed);
 		
 	int c;
 	while((c = getopt (argc, argv, "vs:i:p:h")) != -1)
@@ -311,10 +311,10 @@ int main(int argc, char** argv)
 				abort();
 		}	
 	}
-    
+	
 
-    size_t global_size;
-    size_t local_size;
+	size_t global_size;
+	size_t local_size;
 
 	
 	h_num = malloc(sizeof(*h_num) * N);
@@ -324,21 +324,21 @@ int main(int argc, char** argv)
 	//Generate Tables for the given size of N
 	int numTables = floor(log(N)/log(2)) + 1;
 	printf("num tables = %d\n", numTables);
-	unsigned char* tables = malloc(256 * sizeof(char) * numTables);	
-    
+	h_tables = malloc(256 * sizeof(char) * numTables);	
+	
 	// Create the input and output arrays in device memory for our calculation
-    dev_input = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(char)*N, NULL, &err);
-    CHKERR(err, "Failed to allocate device memory!");
-    dev_table = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(char)*256 * numTables, NULL, &err);
-    CHKERR(err, "Failed to allocate device memory!");
-    dev_output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(char)*N, NULL, &err);
-    CHKERR(err, "Failed to allocate device memory!");
+	dev_input = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(char)*N, NULL, &err);
+	CHKERR(err, "Failed to allocate device memory!");
+	dev_table = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(char)*256 * numTables, NULL, &err);
+	CHKERR(err, "Failed to allocate device memory!");
+	dev_output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(char)*N, NULL, &err);
+	CHKERR(err, "Failed to allocate device memory!");
 
-	computeTables(tables, numTables, crc);
-    
+	computeTables(h_tables, numTables, crc);
+	
 	// Write our data set into the input array in device memory
-    err = clEnqueueWriteBuffer(commands, dev_table, CL_TRUE, 0, sizeof(char)*256*numTables, tables, 0, NULL, NULL);
-    CHKERR(err, "Failed to write to source array!");
+	err = clEnqueueWriteBuffer(commands, dev_table, CL_TRUE, 0, sizeof(char)*256*numTables, tables, 0, NULL, NULL);
+	CHKERR(err, "Failed to write to source array!");
 	
 	//Open file if it exists	
 	if(file != NULL)
@@ -365,6 +365,7 @@ int main(int argc, char** argv)
 	if(fp != NULL)
 		fclose(fp);
 	free(h_num);
+	free(h_tables);
  
 	// Calculate the result if done in serial to verify that we have the correct answer.
 	if(run_serial)
@@ -375,9 +376,9 @@ int main(int argc, char** argv)
 		{
 			srand(seed);
 			h_num = malloc(sizeof(*h_num) * count);
-		    int i;
-		    for(i = 0; i < count; i++)
-		        h_num[i] = rand();
+			int i;
+			for(i = 0; i < count; i++)
+				h_num[i] = rand();
 		}
 		else
 		{
@@ -391,8 +392,8 @@ int main(int argc, char** argv)
 			size_t read = fread(h_num, 1, DATA_SIZE, fp);
 			count = read;// + pad;
 		}
-    	unsigned char crcVal = serialCRC(h_num, count, crc);
-    	printf("Serial Computation: '%X'\n", crcVal);
+		unsigned char crcVal = serialCRC(h_num, count, crc);
+		printf("Serial Computation: '%X'\n", crcVal);
 		free(h_num);
 		if(fp)
 			fclose(fp);
