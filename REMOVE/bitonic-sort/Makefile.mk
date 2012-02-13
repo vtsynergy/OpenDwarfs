83f87ad2bb84b0ae56566f6d5afc8b571e87f4e4
Paul Sathre <sath6220@vt.edu>
2012-02-13 13:21:58 -0500

    Added SHOC licenses for clFFT. Moved bitonic and odd-even sorts to ./REMOVE as removal candidates.

---
+++ REMOVE/bitonic-sort/Makefile.mk
@@ -0,0 +1,19 @@
+#
+# Copyright 2010 by Virginia Polytechnic Institute and State
+# University. All rights reserved. Virginia Polytechnic Institute and
+# State University (Virginia Tech) owns the software and its
+# associated documentation.
+#
+
+bin_PROGRAMS += bsort
+
+bsort_SOURCES = graph-traversal/bitonic-sort/main.c
+
+all_local += bitonic-sort-all-local
+exec_local += bitonic-sort-exec-local
+
+bitonic-sort-all-local:
+	cp $(top_srcdir)/graph-traversal/bitonic-sort/sort.cl .
+
+bitonic-sort-exec-local:
+	cp $(top_srcdir)/graph-traversal/bitonic-sort/sort.cl ${DESTDIR}${bindir}
